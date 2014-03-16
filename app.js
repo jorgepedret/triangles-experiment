a2b5115686b3af064aa6a7b4ffb68d2a1d549954
Jorge Pedret <jorge@chloi.io>
2014-03-15 18:23:44 -0700

    Added configuration matrix for triangles

--- app.js
+++ app.js
@@ -6,24 +6,40 @@
     var vpWidth = $(window).outerWidth();
     var vpHeight = $(window).outerHeight();
 
-    s.path("M 0 0 L " + vpWidth/2 + " 0 L 0 " + vpHeight + " L 0 0").attr({
-        fill: "crimson",
-        strokeWidth: 5
-    });
-    
-    s.path("M " + vpWidth/2 + " 0 L " + vpWidth + " " + vpHeight + " L 0 " + vpHeight + " L " + vpWidth/2 + " 0").attr({
-        fill: "green",
-        strokeWidth: 5
-    });
+    var triangles = [
+      {
+        points: [[0,0], [vpWidth/2, 0], [0, vpHeight]],
+        attr: {
+          fill: "crimson",
+          strokeWidth: 5
+        }
+      },
+      {
+        points: [[vpWidth/2,0], [vpWidth, vpHeight], [0, vpHeight]],
+        attr: {
+          fill: "orange",
+          strokeWidth: 5
+        }
+      },
+      {
+        points: [[vpWidth/2,0], [vpWidth, 0], [vpWidth, vpHeight]],
+        attr: {
+          fill: "green",
+          strokeWidth: 5
+        }
+      }
+    ];
 
-    s.path("M " + vpWidth/2 + " 0 L " + vpWidth + " 0 L " + vpWidth + " " + vpHeight + " L " + vpWidth/2 + " 0").attr({
-        fill: "orange",
-        strokeWidth: 5
-    });
+    var makeTriangle = function(points, attr) {
+      return s.path("M " + points[0][0] + " " + points[0][1] + " L " + points[1][0] + " " + points[1][1] + " L " + points[2][0] + " " + points[2][1] + " L " + points[0][0] + " " + points[0][1]).attr(attr);
+    }
 
-    console.log(path);
+    for (var i in triangles) {
+      t = triangles[i];
+      triangles[i].path = makeTriangle(t.points, t.attr);
+    }
+
   }
-
 
   window.onload = init
 })()

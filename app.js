0284689cc4be45185540ca174e29e70d34867b1b
Jorge Pedret <jorge@chloi.io>
2014-03-20 23:15:23 -0700

    Made it work

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.round(Math.random() * 15)];
+    }
+    return color;
+}
+
 (function(){
   
   var init = function(){
@@ -6,38 +15,99 @@
     var vpWidth = $(window).outerWidth();
     var vpHeight = $(window).outerHeight();
 
-    var triangles = [
+    /*
+     * Initial two triangles configuration object
+     */
+    var initialTriangles = [
       {
-        points: [[0,0], [vpWidth/2, 0], [0, vpHeight]],
+        points: [       // First triangle
+          [0,0],        // First point
+          [vpWidth, 0], // Second point
+          [0, vpHeight] // Third point
+        ],
         attr: {
-          fill: "crimson",
-          strokeWidth: 5
+          fill: getRandomColor()
         }
       },
       {
-        points: [[vpWidth/2,0], [vpWidth, vpHeight], [0, vpHeight]],
+        points: [               // Second triangle
+          [vpWidth,0],          // First point
+          [vpWidth, vpHeight],  // Second point
+          [0,vpHeight]          // Third point
+        ],
         attr: {
-          fill: "orange",
-          strokeWidth: 5
-        }
-      },
-      {
-        points: [[vpWidth/2,0], [vpWidth, 0], [vpWidth, vpHeight]],
-        attr: {
-          fill: "green",
-          strokeWidth: 5
+          fill: getRandomColor()
         }
       }
     ];
 
-    var makeTriangle = function(points, attr) {
-      return s.path("M " + points[0][0] + " " + points[0][1] + " L " + points[1][0] + " " + points[1][1] + " L " + points[2][0] + " " + points[2][1] + " L " + points[0][0] + " " + points[0][1]).attr(attr);
+    /*
+     * Function that takes three points (points) and a point (x,y) inside the triangle
+     * Responsible for determining the new three triangles that are going
+     * to be generated
+     *
+     * Parameters:
+     * `points` is the points of the triangle that was clicked
+     * `x` is the x position of the mouse
+     * `y` is the y position of the mouse
+     */
+    var splitTriangle = function (points, x, y) {
+
+      var triangles = [
+        // First triangle
+        {
+
+          points: [[x,y],[points[0][0],points[0][1]],[points[1][0],points[1][1]]],
+          attr: {
+            fill: getRandomColor()
+          }
+        },
+        // Second triangle
+        {
+          points: [[x,y],[points[0][0],points[0][1]],[points[2][0],points[2][1]]],
+          attr: {
+            fill: getRandomColor()
+          }
+        },
+        // Third triangle
+        {
+          points: [[x,y],[points[1][0],points[1][1]],[points[2][0],points[2][1]]],
+          attr: {
+            fill: getRandomColor()
+          }
+        }
+      ]
+      makeTriangles(triangles)
     }
 
-    for (var i in triangles) {
-      t = triangles[i];
-      triangles[i].path = makeTriangle(t.points, t.attr);
+    /*
+     * Makes ONE triangle
+     */
+    var makeTriangle = function (points, attr) {
+      var path = s.path("M " + points[0][0] + " " + points[0][1] + " L " + points[1][0] + " " + points[1][1] + " L " + points[2][0] + " " + points[2][1] + " L " + points[0][0] + " " + points[0][1]).attr(attr);
+      
+      /*
+       * Whenever I click on this path (triangle) do something...
+       * in this case is splitTriangle
+       */
+      path.click(function(e, x, y){
+        splitTriangle(points, x, y);
+      })
+
+      return path;
     }
+
+    /*
+     * 
+     */
+    var makeTriangles = function(triangles) {
+      for (var i in triangles) {
+        t = triangles[i];
+        triangles[i].path = makeTriangle(t.points, t.attr);
+      }
+    }
+
+    makeTriangles(initialTriangles);
 
   }
 
